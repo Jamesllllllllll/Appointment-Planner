db38070d1a4c742c09b17d23bfe4991e53bed760
James <jkeezer@pm.me>
2023-04-24 15:21:22 -0400

    Handlers for form fields, addContact, clear/submit

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,12 +3,11 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-
 export const ContactsPage = (props) => {
-  const [ contactName, setContactName ] = useState("");
-  const [ contactPhone, setContactPhone ] = useState("");
-  const [ contactEmail, setContactEmail ] = useState("");
-  
+  const [contactName, setContactName] = useState("");
+  const [contactPhone, setContactPhone] = useState("");
+  const [contactEmail, setContactEmail] = useState("");
+
   const { contacts, addContact } = props;
   /*
   Define state variables for 
@@ -17,10 +16,19 @@ export const ContactsPage = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let newName = (e.target.value);
+    let newName = e.target.value;
     if (!contacts.find(newName)) {
       // Add to contacts list
-      
+      const newContact = {
+        /* id: --create generateId() */
+        name: contactName,
+        phone: contactPhone,
+        email: contactEmail,
+      };
+      addContact(newContact);
+      setContactName(""); // can I set all on one line?
+      setContactPhone("");
+      setContactPhone("");
     }
     /*
     Add contact info and clear data
@@ -28,13 +36,20 @@ export const ContactsPage = (props) => {
     */
   };
 
-
   const handleNameChange = (e) => {
-    let newName = (e.target.value);
+    let newName = e.target.value;
     if (contacts.find(newName)) {
-      console.log('Name is a duplicate');
+      console.log("Name is a duplicate");
     }
     setContactName(newName);
+  };
+
+  const handlePhoneChange = (e) => {
+    setContactPhone(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setContactEmail(e.target.value);
   };
 
   /*
@@ -45,14 +60,30 @@ export const ContactsPage = (props) => {
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <ContactForm />
         {/* I think add <ContactForm /> instead of form here */}
         <form className="AddContactForm" onSubmit={handleSubmit}>
-          <input value={contactName}
-          onChange={handleNameChange}
-          type="text"
-          aria-label="Your name"
-          placeholder="Your name"
+          <input
+            value={contactName}
+            onChange={handleNameChange}
+            type="text"
+            aria-label="Your name"
+            placeholder="Your name"
+          />
+          <input
+            value={contactPhone}
+            onChange={handlePhoneChange}
+            type="number"
+            aria-label="Your phone number"
+            placeholder="Your phone number"
+          />
+          <input
+            value={contactEmail}
+            onChange={handleEmailChange}
+            type="email"
+            aria-label="Your email"
+            placeholder="Your email"
           />
         </form>
       </section>
@@ -61,7 +92,12 @@ export const ContactsPage = (props) => {
         <h2>Contacts</h2>
         <ul>
           {contacts.map((contact) => (
-            <TileList key={contact.contactId} contactName={contact.name} contactPhone={contact.phone} contactEmail={contact.email} />
+            <TileList
+              key={contact.contactId}
+              contactName={contact.name}
+              contactPhone={contact.phone}
+              contactEmail={contact.email}
+            />
           ))}
         </ul>
       </section>
