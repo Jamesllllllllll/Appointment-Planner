ab680b30c3858b4ffcb36f457adad0f76809b726
James <jkeezer@pm.me>
2023-04-26 01:34:51 -0400

    ContactPicker working

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -5,7 +5,7 @@ export const ContactPicker = ({onChange, contacts, value}) => {
   return (
     
     <select name="contacts" id="contacts" onChange={onChange} >
-      <option value="name">Name</option>
+      <option value="">No contact selected</option>
       {contacts.map(({name}) => (
         <option value={name}>{name}</option>
       ))}
