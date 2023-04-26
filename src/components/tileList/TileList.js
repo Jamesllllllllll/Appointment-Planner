4cca319b1c621fb4f746d488d667a039d9f6ff50
James <jkeezer@pm.me>
2023-04-26 01:51:13 -0400

    Styling on Tiles

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -5,10 +5,10 @@ export const TileList = (props) => {
   const { tiles } = props;
 
   return (
-    <>
+    <div class="tileList-container">
       {tiles.map(({id, name, ...rest}) => (
         <Tile key={id} name={name} description={rest} />
       ))}
-    </>
+    </div>
   );
 };
